452777cf8c0b6eb2c33d03a83b96fc599040c696
Max16 <max16@naver.com>
2021-06-25 16:41:40 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  // private : 외부에서 변경 및 호출 불가
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "max",
-  gender: "male",
-  age: 22,
-};
+const max = new Human("max", 20, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(max));
 
 export {};
